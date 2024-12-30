fc82301696163db4c9cb394464f03afbf91a13d3
moa2587 <moa25874389@gmail.com>
2024-12-30 15:12:23 +0330

    wwmoa_edit3

--- WallWizard.py
+++ WallWizard.py
@@ -37,19 +37,47 @@ def find_piece(x):
                 return([i, j])
 ############################## تابع چک کردن حرکت مهره x
 def check_move(x):
-    available = [True, True, True, True, True, True, True, True]
+    available = [True, True, True, True, "Player", True, True, True, True]
     place = find_piece(x)
     a = place[0]
     b = place[1]
+############### (333)
     if(b == 0):
         available[3] = False
     else:
-        if(table_piece[a][b-1] == 0):
-            if(table_wall_y[a][b-1] == 1): available[3] = False
-        else:
+        if(table_wall_y[a][b-1] == 1): available[3] = False
+        if(table_piece[a][b-1] != 0):
             if(b-1 == 0):
                 available[3] = False
             elif(table_wall_y[a][b-2] == 1): available[3] = False
+############### (555)
+    if(b == n-1):
+        available[5] = False
+    else:
+        if(table_wall_y[a][b] == 1): available[5] = False
+        if(table_piece[a][b+1] != 0):
+            if(b+1 == n-1):
+                available[5] = False
+            elif(table_wall_y[a][b+1] == 1): available[5] = False
+############### (111)    
+    if(a == 0):
+        available[1] = False
+    else:
+        if(table_wall_x[a-1][b] == 1): available[1] = False
+        if(table_piece[a-1][b] != 0):
+            if(a-1 == 0):
+                available[1] = False
+            elif(table_wall_x[a-2][b] == 1): available[1] = False
+############### (777)
+    print(a, b)
+    if(a == n-1):
+        available[7] = False
+    else:
+        if(table_wall_x[a][b] == 1): available[7] = False
+        if(table_piece[a+1][b] != 0):
+            if(a+1 == n-1):
+                available[7] = False
+            elif(table_wall_x[a+1][b] == 1): available[7] = False
     return available
 ##############################
 def do_something(x):
@@ -58,10 +86,9 @@ def do_something(x):
     if(s == '1'): print(0)
     else: print(1)
 
+print(find_piece(2))
+print(check_move(2))
 for i in range(n):
     for j in range(n):
         print(table_piece[i][j], end = " ")
     print()
-
-print(find_piece(2))
-print(check_move(2))
